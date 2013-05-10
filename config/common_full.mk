25f56bc9a1d58a52d77dbdf0169d3bdb520e7ae5
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-05-10 20:22:36 +0200

    Remove VideoEditor and example files
    Change-Id: Ief2c54d32b86088b66252d44ebc4e3e377635135

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,9 +7,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # Extra Ringtones
 include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
@@ -24,11 +21,3 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     VisualizationWallpapers \
     PhotoTable
-
-PRODUCT_PACKAGES += \
-    VideoEditor \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
